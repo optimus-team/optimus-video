1b092d704bd9d2d0ae8ec93bdd35dcf58af4d98e
Dmitry <dimasavva17@gmail.com>
2015-09-26 23:37:09 +0300

    Format

--- frontend/main.py
+++ frontend/main.py
@@ -23,7 +23,8 @@ def update_pid():
 	for pid in pids:
 		id,path,ts = pid
 		print id
-		p = subprocess.Popen("ps -p %s" %id,stdout=PIPE,stderr=PIPE)
+		cmd = "ps -p %s" %id
+		p = subprocess.Popen(cmd,shell=True,stdout=PIPE,stderr=PIPE)
 		out,err = p.communicate()
 		if not (id in out):
 			cmd = "avconv -ss 00:00:02 -i %s/%s.mp4 -vsync 1 -t 0.01 out.jpg" % (path,ts)
