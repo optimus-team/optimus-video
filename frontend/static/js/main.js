09945bac90e01290486086e1e43b77d71c8831fc
Gheorghe <Feross95@gmail.com>
2015-09-27 11:41:25 +0300

    pop

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -208,4 +208,24 @@ jQuery(document).ready(function(){
 	} else {
 	    BV.show('../static/video/background.mp4',{ambient:true});
 	}
+    
+    //open popup
+	$('.cd-popup-trigger').on('click', function(event){
+		event.preventDefault();
+		$('.cd-popup').addClass('is-visible');
+	});
+	
+	//close popup
+	$('.cd-popup').on('click', function(event){
+		if( $(event.target).is('.cd-popup-close') || $(event.target).is('.cd-popup') ) {
+			event.preventDefault();
+			$(this).removeClass('is-visible');
+		}
+	});
+	//close popup when clicking the esc keyboard button
+	$(document).keyup(function(event){
+    	if(event.which=='27'){
+    		$('.cd-popup').removeClass('is-visible');
+	    }
+    });
 });
